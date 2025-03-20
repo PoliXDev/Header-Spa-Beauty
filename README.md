e3db2e7bba25e21acdee0b345f8af6628775ed47
DANIEL ALEJANDRO RUIZ POLI <danielruiz368@gmail.com>
2025-03-20 05:25:46 +0100

    Update code and styles

--- README.md
+++
@@ -1,2 +0,0 @@
-# Header Spa-Beauty
- Header Spa & Beauty
---
+++ src/main.js
@@ -0,0 +1,5 @@
+// import "./styles/main.css";
+// import "./styles/styles.css";
+
+// Importamos el archivo CSS unificado
+import "../style-unified.css";
